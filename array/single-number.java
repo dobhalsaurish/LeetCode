ef2ab6610ae572c1f32bdc15a0d36e8775fc5edd
dobhalsaurish <saurishdobhal@gmail.com>
2026-07-09 20:54:01 +0530

    LC-single-number [Runtime: N/A; Memory: N/A]

---
+++ array/single-number.java
@@ -0,0 +1,10 @@
+// Using Bit Manipulation -> XOR '^'
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int num:nums){
+            ans^=num;
+        }
+        return ans;
+    }
+}
